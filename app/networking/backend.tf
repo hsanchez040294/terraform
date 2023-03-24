9de33bd9982877c7ff6da3eb35eb0dd7f4e9835b
hsanchez040294 <128844998+hsanchez040294@users.noreply.github.com>
2023-03-24 13:35:05 -0600

    Update backend.tf

--- app/networking/backend.tf
+++ app/networking/backend.tf
@@ -1,10 +1,10 @@
 terraform {
   backend "s3" {
-      bucket = "tfbackend35517"
+      bucket = "tf-backend-locks97194"
       key = "networking/terraaform.tfstate"
       region = "us-east-2"
 
-      dynamodb_table = "tfbacklocks35517"
+      dynamodb_table = "tf-backend-locks97194"
       encrypt = true
   }
 } 
